f39e164c7cda999825517af95a55a6bfdea5158e
Stalowy-Cezary <dravenop@gmail.com>
2021-12-02 13:44:38 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import random
+import pandas as pd
+def sort(file):
+
+    data = pd.read_csv(str(file), header=None, na_values=" NaN")
+    data = data[data[0].notna()]
+    mylist = data[0].tolist()
+    random.shuffle(mylist)
+    print(mylist)
+    for i in range(len(mylist)-1):
+        print(mylist[i] + " + " + mylist[i+1])
+    print(mylist[-1] + " + " + mylist[0])
+
+x = str(input('enter file name:'))
+
+sort(x)
+
+
+
